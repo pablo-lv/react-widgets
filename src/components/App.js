76e7bb6cf9fc40af581f241d95a1d4f32f9632f5
pablo-lucas <pablo.lucv@gmail.com>
2022-08-01 20:10:36 -0500

    Added Dropdown Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import Accordion from "./Accordion";
+import Dropdown from "./Dropdown";
 import Search from "./Search";
 
 const items = [
@@ -12,11 +14,33 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: "The color Red",
+    value: "red",
+  },
+  {
+    label: "The color Green",
+    value: "green",
+  },
+  {
+    label: "The color Blue",
+    value: "blue",
+  },
+];
+
 function App() {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div className="ui container">
       <Accordion items={items} />
       <Search />
+      <Dropdown
+        options={options}
+        selected={selected}
+        onSelectedChange={setSelected}
+      />
     </div>
   );
 }
